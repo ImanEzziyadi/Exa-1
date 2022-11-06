7c2e2604e30d018b1ace8ace753e2adcebefa921
unknown <emanelzayadi0@gmail.com>
2022-11-06 19:35:35 +0100

    first commit

---
+++ my-cv/src/components/pages/Aboutme.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Aboutme = () => {
+  return (
+    <div>
+    <h1>Aboutme</h1>
+    <h4>I would describe myself as a highly driven, social and creative people person who is  passionate about personal development.</h4>
+    </div>
+  )
+}
+
+export default Aboutme
